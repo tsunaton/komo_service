836e25bce95ea5f43a9af4789f1caaf22abe53ad
lee sooryung <tsuna.tonight@gmail.com>
2019-02-28 22:20:25 +0900

    for mentering

--- app/models/shift.rb
+++ app/models/shift.rb
@@ -1,23 +1,9 @@
 class Shift < ApplicationRecord
-  # ATTRIBUTES = %i(
-  #   start_time
-  #   end_time
-  #     )
 
-  # attr_accessor(*ATTRIBUTES)
-
-  def initialize(start_time, end_time)
-    @start_time = start_time
-    @end_time = end_time
-  end
-
-  def matches
+  def matches(start_time, end_time)
     results = Shift.all
-    binding.pry
-    results = results.where("start_time >= ?", @start_time)#より後
-    results = results.where("end_time <= ?", @end_time)#より前
-
-    return results
+    results = results.where("start_time >= ?", start_time)
+    results = results.where("end_time <= ?", end_time)
 
   end
 
