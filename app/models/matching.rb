363403a86d12577c19b6f291cc0bfdb94991f5a3
lee sooryung <tsuna.tonight@gmail.com>
2019-04-19 20:18:00 +0900

    validationを書く

--- app/models/matching.rb
+++ app/models/matching.rb
@@ -1,4 +1,9 @@
 class Matching < ApplicationRecord
   belongs_to :user
   belongs_to :funeral
+
+  with_options presence: true do
+    validates :user_id
+    validates :funeral_id
+  end
 end
--- app/models/working_hour.rb
+++ app/models/working_hour.rb
@@ -1,3 +1,8 @@
 class WorkingHour < ApplicationRecord
 belongs_to :user
+
+with_options presence: true do
+  validates :user_id
+  validates :start_time
+end
 end
