625b82747537d225be124e0e8e5d718f4cafa743
lee sooryung <tsuna.tonight@gmail.com>
2019-04-30 16:26:39 +0900

    選択画面を作成

--- app/controllers/staff/shifts_controller.rb
+++ app/controllers/staff/shifts_controller.rb
@@ -10,6 +10,18 @@ before_action :logged_in_user, only: [:new, :edit, :update, :destroy]
 
   def new
     @shift = Shift.new
+    t = Date.today
+    @days = (t..(t + 7)).to_a
+
+    @hours = []
+    @days.each do |day|
+      @hoge = []
+      (7..22).each do |t|
+        hour = Shift.new(scheduled_from: "#{day} #{t}:00:00", scheduled_to: "#{day} #{t+1}:00:00", user_id: 1)
+        @hoge.append(hour)
+      end
+      @hours.append(@hoge)
+    end
   end
 
   def edit
