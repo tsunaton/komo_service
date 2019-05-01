6e50ed5c5519eeb130f3a4391782dfa5037e9fad
lee sooryung <tsuna.tonight@gmail.com>
2019-05-01 20:57:57 +0900

    シフト入力から登録までを実装

--- app/controllers/staff/shifts_controller.rb
+++ app/controllers/staff/shifts_controller.rb
@@ -1,5 +1,5 @@
 class Staff::ShiftsController < Staff::ApplicationController
-before_action :logged_in_user, only: [:new, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:new, :edit, :update, :destroy]
 
   def index
     @shifts = Shift.where(user_id: @current_user)
@@ -10,6 +10,9 @@ before_action :logged_in_user, only: [:new, :edit, :update, :destroy]
 
   def new
     @shift = Shift.new
+    t = Date.today
+    @days = (t..(t + 7)).to_a
+    @times = (7..22)
   end
 
   def edit
@@ -17,13 +20,8 @@ before_action :logged_in_user, only: [:new, :edit, :update, :destroy]
   end
 
   def create
-    @shift = Shift.new(shift_params)
-    @shift.user_id = @current_user.id
-    if @shift.save
-      redirect_to action: 'index'
-    else
-      render :new
-    end
+    organize_and_integrate(shift_params)
+    redirect_to staff_shifts_path
   end
 
   def update
@@ -40,9 +38,51 @@ before_action :logged_in_user, only: [:new, :edit, :update, :destroy]
   end
 
   private
-    def shift_params
-      params
-        .require(:shift)
-        .permit(:start_time, :end_time)
+
+  def shift_params
+    @scheduled_from_params = []
+    (0..7).each do |i|
+      (0..15).each do |i_2|
+        scheduled_from = "scheduled_from#{i}-#{i_2}"
+        @scheduled_from_params.append(scheduled_from)
+      end
     end
+    params
+      .require(:shift)
+      .permit(@scheduled_from_params)
+  end
+
+  def organize_and_integrate(shift_params)
+    @from = nil
+    shift_params.values.each_with_index do |t, i|
+      /(.+):(\d+)/ =~ t
+      organize_and_integrate_helper($1, $2, i)
+    end
+  end
+
+  def organize_and_integrate_helper(date, time, i)
+    #最後にポツンとひとつなら、そのまま保存する。
+    if @from == nil && i == shift_params.values.length - 1
+      render :new unless @shift.save
+      @shift = Shift.create(scheduled_from: "#{date} #{time}:00:00", scheduled_to: "#{date} #{time.to_i + 1}:00:00", user_id: @current_user.id)
+    #開始時間が未定義なら、開始時間を定義する。
+    elsif @from == nil
+      @from = time.to_i
+      @to = time.to_i + 1
+      @shift = Shift.new(scheduled_from: "#{date} #{@from}:00:00", scheduled_to: "#{date} #{@to}:00:00", user_id: @current_user.id)
+    #開始時間がさっきの1時間後かつ同じ日付なら、ひとつ前の終了時間を1時間伸ばす。かつ最後のひとつなら保存する。
+    elsif time.to_i == @to && shift_params.values[i-1].include?(date)
+      @to = @to + 1
+      @shift = Shift.new(scheduled_from: "#{date} #{@from}:00:00", scheduled_to: "#{date} #{@to}:00:00", user_id: @current_user.id)
+      if i == shift_params.values.length - 1
+        render :new unless @shift.save
+        @from = nil
+      end
+    #どれにも当てはまらなければ、１つ前にnewしてあったのを保存し、@fromを空にした後、再びこのメソッドを呼ぶ。
+    else
+      render :new unless @shift.save
+      @from = nil
+      organize_and_integrate_helper(date, time, i)
+    end
+  end
 end
