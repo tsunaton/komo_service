c694c969ee9ebfb8c7f6d290cb8ab6d818b1de52
lee sooryung <tsuna.tonight@gmail.com>
2019-05-03 19:08:32 +0900

    承認・辞退のビュー・コントローラを実装

--- app/controllers/staff/working_hours_controller.rb
+++ app/controllers/staff/working_hours_controller.rb
@@ -1,22 +1,42 @@
-class Staff::FuneralingHoursController < Staff::ApplicationController
-before_action :set_user, only: [:show, :edit, :update, :destroy]
+class Staff::WorkingHoursController < Staff::ApplicationController
 
-  #終了報告
   def new
   end
 
   def create
   end
 
-  #遅刻処理
   def edit
+    @working_hour = WorkingHour.find(params[:id])
   end
 
   def update
+    working_hour = WorkingHour.find(params[:id])
+    funeral = Funeral.find(working_hour.funeral_id)
+
+    case working_hour_params[:commit]
+    when "受ける"
+      if working_hour.update(start_time: funeral.start_time, status: "accepted")
+        redirect_to staff_home_path
+      else
+        render :edit
+      end
+    when "辞退する"
+      if working_hour.update(status: "rejected")
+        redirect_to staff_home_path
+      else
+        render :edit
+      end
+    end
   end
 
-  #間違ったとき削除
   def destroy
+  end
+
+  private
+
+  def working_hour_params
+    params.permit(:commit)
   end
 
 end
