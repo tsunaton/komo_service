8a37b8430a746d987f232c7642dcf860d5e3705e
lee sooryung <tsuna.tonight@gmail.com>
2019-05-05 14:17:13 +0900

    不必要なseedの記述を削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-#Funeralをいっぱい作る
-6.times do |i|
-  unless i == 0
-    start_time = "2019-03-1#{i} 0#{i}:00:00"
-    end_time = "2019-03-1#{i} 1#{i}:00:00"
-    Funeral.create(:id => i, :start_time => start_time, :end_time => end_time, :number_of_people => i, :funeral_halls_id => i, :client_id => i)
-  end
-end
-
-#Machingをいっぱい作る
-10.times do |i|
-  unless i == 0
-    Maching.create(:id => i, :shift_id => rand(10..13), :funeral_id => rand(1..5))
-  end
-end
