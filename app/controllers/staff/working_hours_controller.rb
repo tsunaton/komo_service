bdd4ca44051c9c5bbc2b1a2e9cabcac4dcc78802
lee sooryung <tsuna.tonight@gmail.com>
2019-05-07 15:24:36 +0900

    リファクタリング

--- app/controllers/staff/working_hours_controller.rb
+++ app/controllers/staff/working_hours_controller.rb
@@ -10,24 +10,14 @@ class Staff::WorkingHoursController < Staff::ApplicationController
     @working_hour = WorkingHour.find(params[:id])
 
     # 15分未満を切り捨てる
-    min = Time.now.min
-    if min >= 00 && min < 15
-      @t = Time.now.change(min: 00)
-      elsif min >= 15 && min < 30
-      @t = Time.now.change(min: 15)
-      elsif min >= 30 && min < 45
-      @t = Time.now.change(min: 30)
-      elsif min >=45
-      @t = Time.now.change(min: 45)
-    end
+    @t = Time.at((Time.now.to_f / 15.minutes).round * 15.minutes)
   end
 
   def update
     working_hour = WorkingHour.find(params[:id])
-    funeral = Funeral.find(working_hour.funeral_id)
     case working_hour_params[:commit]
     when "受ける"
-      if working_hour.update(start_time: funeral.start_time, status: "accepted")
+      if working_hour.update(start_time: working_hour.funeral.start_time, status: "accepted")
         redirect_to staff_home_path
       else
         render :edit
@@ -36,9 +26,9 @@ class Staff::WorkingHoursController < Staff::ApplicationController
       if working_hour.update(status: "rejected")
         redirect_to staff_home_path
       else
-
+        render :edit
       end
-    when nil #終了報告時
+    else
       if working_hour.update(end_time: working_hour_params[:end_time])
         redirect_to staff_home_path
       else
