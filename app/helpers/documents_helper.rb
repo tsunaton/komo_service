8a5462770fb8ab385bd51f55573255d2ef494fbd
lee sooryung <tsuna.tonight@gmail.com>
2019-05-09 12:53:05 +0900

    リファクタリング

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,9 +1,7 @@
 module DocumentsHelper
   def calculate_working_hour(work)
-    if work.status == "done"
-      working_hours = (work.end_time - work.start_time) / 3600
-      working_hours.ceil
-    end
+    working_hours = (work.end_time - work.start_time) / 3600
+    working_hours.ceil
   end
 
   def calculate_pay_per_day(work, per_hour)
@@ -13,16 +11,5 @@ module DocumentsHelper
 
   def calculate_monthly(works, per_hour)
     works.inject(0){ |sum, w| sum += calculate_pay_per_day(w, per_hour) }
-  end
-
-  def calculate_pay_per_this_month_for_client(client)
-    funerals = Funeral.where(client_id: client.id)
-
-    funerals.each { |f| @working_hours = Array(nil) << WorkingHour.find(f.id)}
-    @working_hours
-    
-    @sum = 0
-    @working_hours.each { |w| @sum += calculate_pay_per_day(w) }
-    @sum
   end
 end
