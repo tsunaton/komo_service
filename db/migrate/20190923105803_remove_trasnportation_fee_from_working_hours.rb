f80ca3ef607633254f307a3b39717980522b294f
lee sooryung <tsuna.tonight@gmail.com>
2019-11-02 19:50:29 +0900

    modify migration file

--- db/migrate/20190923105803_remove_trasnportation_fee_from_working_hours.rb
+++ db/migrate/20190923105803_remove_trasnportation_fee_from_working_hours.rb
@@ -1,4 +1,4 @@
-class RemovetransportationFeeFromWorkingHours < ActiveRecord::Migration[5.2]
+class RemoveTransportationFeeFromWorkingHours < ActiveRecord::Migration[5.2]
   def change
     remove_column :working_hours, :transportation_fee, :integer
   end
