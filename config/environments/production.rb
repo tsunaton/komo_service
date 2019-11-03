fe556af8e0a9ebe60aea4c90bb78af5bad3be671
lee sooryung <tsuna.tonight@gmail.com>
2019-11-03 16:56:34 +0900

    herokuのために

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,10 +95,17 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    port: 587,
-    user_name: 'leetanshim@gmail.com',
-    password: 'rzrjsrdahverzfqu',
-    authentication:       'plain'}
+  # config.action_mailer.smtp_settings = {
+  #   address: "smtp.gmail.com",
+  #   port: 587,
+  #   user_name: 'leetanshim@gmail.com',
+  #   password: 'rzrjsrdahverzfqu',
+  #   authentication:       'plain'}
+
+    config.action_mailer.smtp_settings = {
+      address: "smtp.gmail.com",
+      port: 587,
+      user_name: 'tsuna.tonight@gmail.com',
+      password: 'tyuklloosrsgmjry',
+      authentication:       'plain'}
 end
